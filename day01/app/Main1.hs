a57dfc48dbf72ea4edcee1806170af21f4ef1387
mehdidotcafe <contact@mehdi.cafe>
2025-12-02 17:02:02 +0100

    first commit

---
+++ day01/app/Main1.hs
@@ -0,0 +1,19 @@
+module Main (main) where
+
+getSign c = case c of
+                'L' -> -1
+                'R' -> 1
+
+parse = map (\(x:xs) -> getSign x * (read :: String -> Int) xs) . lines
+
+hasMadeClick r pos dc
+  | r > 0 = (r + pos) `div` dc
+  | otherwise = (abs r + ((dc - pos) `mod` dc)) `div` dc
+
+solve pos dc rotations = case rotations of
+  [] -> 0
+  (r:rs) -> let
+    start' = (pos + r) `mod` dc
+    in (if start' == 0 then 1 else 0) + solve start' dc rs
+
+main = getContents >>= print . solve 50 100 . parse
